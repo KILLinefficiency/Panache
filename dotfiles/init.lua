e64818012042c0ad39985a52bd8bc9150f1ae400
KILLinefficiency <shreyas.sable2166@gmail.com>
2026-09-19 15:09:05 +0530

    Added dotfiles.

---
+++ dotfiles/init.lua
@@ -0,0 +1,102 @@
+vim.pack.add {
+    { src = 'https://www.github.com/saghen/blink.cmp' },
+    { src = 'https://www.github.com/saghen/blink.lib' },
+    { src = 'https://www.github.com/mason-org/mason.nvim' },
+    { src = 'https://www.github.com/neovim/nvim-lspconfig' },
+    { src = 'https://www.github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://www.github.com/nvim-tree/nvim-tree.lua' },
+    { src = 'https://www.github.com/nvim-lualine/lualine.nvim' },
+    { src = 'https://www.github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://www.github.com/nvim-telescope/telescope.nvim' },
+    { src = 'https://www.github.com/mason-org/mason-lspconfig.nvim' },
+    { src = 'https://www.github.com/nvim-treesitter/nvim-treesitter' },
+    { src = 'https://www.github.com/catppuccin/nvim', name = 'catppuccin' },
+}
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.encoding = 'UTF-8'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.showmode = true
+vim.opt.showmatch = true
+vim.opt.swapfile = false
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+
+local telescope_builtins = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', telescope_builtins.find_files, { desc = 'Find Files using Telescope' })
+vim.keymap.set('n', '<leader>g', telescope_builtins.live_grep, { desc = 'Live Grep using Telescope' })
+
+local languages = {
+    'cpp',
+    'typst',
+    'python',
+    'haskell',
+    'javascript',
+    'typescript'
+}
+
+require('nvim-treesitter').install(languages)
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = languages,
+    callback = function()
+        pcall(vim.treesitter.start)
+    end
+})
+
+local blink = require('blink.cmp')
+blink.setup({
+    fuzzy = {
+        implementation = 'lua'
+    }
+})
+
+require('mason').setup()
+require('mason-lspconfig').setup {
+    ensure_installed = {
+        'pyright',
+        'clangd',
+        'tinymist'
+    }
+}
+
+local blink_capabilities = blink.get_lsp_capabilities()
+
+vim.lsp.enable('clangd', {
+    capabilities = blink_capabilities,
+    cmd = {
+        'clangd',
+        '--background-index',
+        '--clang-tidy'
+    }
+})
+
+vim.lsp.enable('tinymist', {
+    capabilities = blink_capabilities
+})
+
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' }
+    }
+}
+
+require('nvim-tree').setup()
+vim.keymap.set('n', '<leader>a', ':NvimTreeOpen<CR>', { desc = 'File Browser via NvimTree' })
+
+vim.cmd.colorscheme 'catppuccin-mocha'
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+sudo pacman -S \
+    fd \
+	git \
+	zsh \
+    gcc \
+	tmux \
+	curl \
+    tar \
+    zip \
+    gzip \
+    unzip \
+    typst \
+	neovim \
+    ripgrep \
+	alacritty \
+    tree-sitter \
+    tree-sitter-cli \
+	ttf-jetbrains-mono \
+	ttf-jetbrains-mono-nerd \
+	--noconfirm
+
+sudo chsh -s /usr/bin/zsh $USER
+
+# Alacritty
+mkdir -p ~/.config/alacritty
+cp -v ./dotfiles/alacritty.toml ~/.config/alacritty
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# mkdir -p ~/.config/tmux/plugins/catppuccin
+# git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+cp -v ./dotfiles/tmux.conf ~/.tmux.conf
+
+# Bash
+cp -v ./dotfiles/bashrc ~/.bashrc
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp -v ./dotfiles/zshrc ~/.zshrc
+
+# NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash
+source $HOME/.nvm/nvm.sh
+nvm install --lts
+
+# Neovim
+mkdir -p ~/.config/nvim
+cp -v ./dotfiles/init.lua ~/.config/nvim/init.lua
+nvim --headless +q
